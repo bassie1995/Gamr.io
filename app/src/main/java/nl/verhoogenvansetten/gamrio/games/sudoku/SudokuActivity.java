1324a3fa4d78f9c86a3b79c6347435d12921fbca
Daniel Jansen <djaniboe@gmail.com>
2016-10-29 14:28:27 +0200

    Update Sudoku networking to send board

--- app/src/main/java/nl/verhoogenvansetten/gamrio/games/sudoku/SudokuActivity.java
+++ app/src/main/java/nl/verhoogenvansetten/gamrio/games/sudoku/SudokuActivity.java
@@ -3,13 +3,16 @@ package nl.verhoogenvansetten.gamrio.games.sudoku;
 import android.app.DialogFragment;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.GridLayout;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import nl.verhoogenvansetten.gamrio.GameCompat;
 import nl.verhoogenvansetten.gamrio.R;
+import nl.verhoogenvansetten.gamrio.util.StaticGameData;
 import nl.verhoogenvansetten.gamrio.util.network.Network;
 
 public class SudokuActivity extends GameCompat implements View.OnClickListener{
@@ -24,19 +27,24 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sudoku);
 
+        network = Network.getInstance();
+        network.registerGame(Network.SUDOKU, this);
+    }
+
+    private void setupSudoku(int[][] s) {
+        sudoku = (s != null ? s : SudokuLogic.removeNumbers(SudokuLogic.generateCompletedSudoku(), 63));
+
         Button array[][] = new Button[9][9];
-        sudoku = SudokuLogic.removeNumbers(SudokuLogic.generateCompletedSudoku(), 63);
 
         final GridLayout g = (GridLayout) findViewById(R.id.sudoku_grid);
         for(int i = 0; i < 81; i++){
             Button newButton;
             if(sudoku[i/9][i%9] != 0){
                 newButton = new Button(this, null, R.attr.borderlessButtonStyle);
+                newButton.setClickable(false);
             }else{
                 newButton = new Button(this);
-                //b.setStateListAnimator(null);
-                //b.setShadowLayer(0, 0, 0, ContextCompat.getColor(this, R.color.md_white));
-                newButton.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.md_amber_50), PorterDuff.Mode.MULTIPLY);
+                newButton.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.md_white), PorterDuff.Mode.MULTIPLY);
             }
 
             GridLayout.LayoutParams p = new GridLayout.LayoutParams();
@@ -45,22 +53,19 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
             p.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
             p.rowSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f);
 
-            //b.setText(String.valueOf(i%9+1));
             newButton.setText(sudoku[i/9][i%9] != 0 ? String.valueOf(sudoku[i/9][i%9]) : "");
             newButton.setId(i+1);
             newButton.setLayoutParams(p);
-            newButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    clickedButton = (Button) v;
-
-                    newFragment = new SudokuNumberFragment();
-                    newFragment.show(getFragmentManager(), "number");
-
-                    //Snackbar.make(g, "Correct: " + SudokuLogic.checkSudoku(sudoku), Snackbar.LENGTH_LONG)
-                    //        .setAction("Action", null).show();
-                }
-            });
+            if(newButton.isClickable()){
+                newButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        clickedButton = (Button) v;
+                        newFragment = new SudokuNumberFragment();
+                        newFragment.show(getFragmentManager(), "number");
+                    }
+                });
+            }
             array[i/9][i%9] = newButton;
         }
 
@@ -69,9 +74,6 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
                 g.addView(b);
             }
         }
-
-        network = Network.getInstance();
-        network.registerGame(Network.SUDOKU, this);
     }
 
     @Override
@@ -82,7 +84,21 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
                 clickedButton = (Button)findViewById(Integer.parseInt(array[1]));
                 changeNumber(Integer.parseInt(array[2]));
                 break;
+            case "CONNECTED":
+                setupSudoku(null);
+                sendSudoku();
+                break;
+            case "SUDOKU":
+                setupSudoku(SudokuLogic.stringToSudoku(array[1]));
+                break;
+            //case "SUDOKU_RECEIVED":
+
+            //    break;
         }
+    }
+
+    private void sendSudoku() {
+        network.send(Network.SUDOKU, "SUDOKU " + SudokuLogic.sudokuToString(sudoku));
     }
 
     @Override
@@ -100,7 +116,7 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
         Button b = (Button) view;
         int input = Integer.parseInt((String)b.getText());
         changeNumber(input);
-        network.send(Network.SUDOKU, "MOVE " + b.getId() + " " + input);
+        network.send(Network.SUDOKU, "MOVE " + clickedButton.getId() + " " + input);
         newFragment.getDialog().dismiss();
     }
 
@@ -108,5 +124,8 @@ public class SudokuActivity extends GameCompat implements View.OnClickListener{
         int id = clickedButton.getId()-1;
         sudoku[id/9][id%9] = i;
         clickedButton.setText(String.valueOf(i));
+        clickedButton.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.md_amber_50), PorterDuff.Mode.MULTIPLY);
     }
+
+
 }
